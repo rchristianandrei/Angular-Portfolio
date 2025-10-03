f7b4e4bb21cddfc96f960eda567344f966400813
Christian Andrei Reyes <98152145+rchristianandrei@users.noreply.github.com>
2025-10-03 09:51:09 +0800

    animation + better bg image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { ThemeToggleComponent } from "./theme-toggle/theme-toggle.component";
-import { IntroductionComponent } from "./introduction/introduction.component";
-import { GalleryComponent } from "./gallery/gallery.component";
-import { ProjectsComponent } from "./projects/projects.component";
-import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from './header/header.component';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { EducationComponent } from "./education/education.component";
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ThemeToggleComponent, IntroductionComponent, GalleryComponent, WorkExperienceComponent, ProjectsComponent, FooterComponent, EducationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ThemeToggleComponent,
+    IntroductionComponent,
+    GalleryComponent,
+    WorkExperienceComponent,
+    ProjectsComponent,
+    FooterComponent,
+    EducationComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-portfolio';
+
+  ngOnInit(): void {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const intersecting = entry.isIntersecting;
+          if (intersecting) {
+            entry.target.classList.add('animate-visible');
+          } else {
+            entry.target.classList.remove('animate-visible');
+          }
+        });
+      },
+      { threshold: 0.25 }
+    );
+
+    Array.from(document.getElementsByClassName('animate-element')).forEach(
+      element => {
+        observer.observe(element);
+      }
+    );
+  }
 }
