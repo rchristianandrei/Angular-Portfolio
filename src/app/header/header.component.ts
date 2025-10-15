7f35c27d4ed07399c8a6d3a97edba4beea069590
Christian Andrei Reyes <careyes@sscgi.com>
2025-10-15 09:28:44 +0800

    add burger menu icon and animation on other sections

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
+import { NgIf } from '@angular/common'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  show = false;
 }
